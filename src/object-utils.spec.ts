72de8eebf72bf70ccd37bfcac1bf0758aad91c1c
David Crush <davidcrushjr@gmail.com>
2022-03-10 23:51:45 -0500

    adding get

--- src/object-utils.spec.ts
+++ src/object-utils.spec.ts
@@ -1,6 +1,169 @@
 import ObjectUtils from './object-utils';
 
 describe('ObjectUtils', () => {
+  describe('get', () => {
+    it('returns undefined if path does not exist', () => {
+      const testObj: Partial<TestObj> = {
+        id: 1,
+        name: 'My Test',
+      };
+
+      const result = ObjectUtils.get(testObj, 'secret');
+
+      expect(result).toBeUndefined();
+    });
+
+    it('returns default value if path does not exist', () => {
+      const testObj: Partial<TestObj> = {
+        id: 1,
+        name: 'My Test',
+      };
+
+      const result = ObjectUtils.get(testObj, 'secret', 'test' as string);
+
+      expect(result).toBe('test');
+    });
+
+    it('returns nested value when exists', () => {
+      const testObj: TestObj = {
+        id: 1,
+        name: 'My Test',
+        secret: 'hello',
+        meta: {
+          name: 'something',
+          date: {
+            string: '2022-04-01',
+          },
+          keywords: ['testing', 'bacon'],
+        },
+        items: [
+          { id: 2 },
+          { id: 3 },
+          { id: 5 },
+        ],
+      };
+
+      const result = ObjectUtils.get(testObj, 'meta.date.string');
+
+      expect(result).toBe('2022-04-01');
+    });
+
+    it('returns undefined if nested property does not exist', () => {
+      const testObj: TestObj = {
+        id: 1,
+        name: 'My Test',
+        secret: 'hello',
+        meta: {
+          name: 'something',
+          date: {
+            string: '2022-04-01',
+          },
+          keywords: ['testing', 'bacon'],
+        },
+        items: [
+          { id: 2 },
+          { id: 3 },
+          { id: 5 },
+        ],
+      };
+
+      const result = ObjectUtils.get(testObj, 'meta.date.milliseconds');
+
+      expect(result).toBeUndefined();
+    });
+
+    it('returns default value if nested property does not exist', () => {
+      const testObj: TestObj = {
+        id: 1,
+        name: 'My Test',
+        secret: 'hello',
+        meta: {
+          name: 'something',
+          date: {
+            string: '2022-04-01',
+          },
+          keywords: ['testing', 'bacon'],
+        },
+        items: [
+          { id: 2 },
+          { id: 3 },
+          { id: 5 },
+        ],
+      };
+
+      const result = ObjectUtils.get(testObj, 'meta.date.milliseconds', 69);
+
+      expect(result).toBe(69);
+    });
+
+    it('returns the value for array indexes', () => {
+      const testObj: TestObj = {
+        id: 1,
+        name: 'My Test',
+        secret: 'hello',
+        meta: {
+          name: 'something',
+          date: {
+            string: '2022-04-01',
+          },
+          keywords: ['testing', 'bacon'],
+        },
+        items: [
+          { id: 2 },
+          { id: 3 },
+          { id: 5 },
+        ],
+      };
+
+      expect(ObjectUtils.get(testObj, 'meta.keywords.0')).toBe('testing');
+      expect(ObjectUtils.get(testObj, 'meta.keywords.1')).toBe('bacon');
+    });
+
+    it('returns undefined if array index does not exist', () => {
+      const testObj: TestObj = {
+        id: 1,
+        name: 'My Test',
+        secret: 'hello',
+        meta: {
+          name: 'something',
+          date: {
+            string: '2022-04-01',
+          },
+          keywords: ['testing', 'bacon'],
+        },
+        items: [
+          { id: 2 },
+          { id: 3 },
+          { id: 5 },
+        ],
+      };
+
+      expect(ObjectUtils.get(testObj, 'meta.keywords.2')).toBeUndefined();
+    });
+
+    it('returns property value nested in array', () => {
+      const testObj: TestObj = {
+        id: 1,
+        name: 'My Test',
+        secret: 'hello',
+        meta: {
+          name: 'something',
+          date: {
+            string: '2022-04-01',
+          },
+          keywords: ['testing', 'bacon'],
+        },
+        items: [
+          { id: 2 },
+          { id: 3 },
+          { id: 5 },
+        ],
+      };
+
+      expect(ObjectUtils.get(testObj, 'items.1.id')).toBe(3);
+    });
+  });
+
   describe('filter', () => {
     it('filters an object down to the specified properties', () => {
       const testObj: TestObj = {
@@ -34,4 +197,17 @@ interface TestObj {
   id: number;
   name: string;
   secret: string;
+  meta?: {
+    name: string,
+    date?: {
+      string: string,
+      milliseconds?: number
+    },
+    keywords: string[]
+  },
+  items?: TestItemObj[]
+}
+
+interface TestItemObj {
+  id: number;
 }
--- src/object-utils.ts
+++ src/object-utils.ts
@@ -1,4 +1,26 @@
 class ObjectUtils {
+  static get<T, R>(obj: T, path: string, defaultValue?: R): R | undefined;
+  static get<T, R>(obj: T, path: string[], defaultValue?: R): R | undefined;
+  static get<T, R>(obj: T, path: string | string[], defaultValue?: R): R | undefined {
+    const pathArr = Array.isArray(path) ? path : path.split('.');
+    let valid = true;
+    let returnValue: R | undefined;
+
+    for (let i = 0; i < pathArr.length && valid; i += 1) {
+      if (i === 0) {
+        returnValue = obj[pathArr[i]];
+      } else {
+        returnValue = returnValue ? returnValue[pathArr[i]] : undefined;
+      }
+
+      if (typeof returnValue === 'undefined' || returnValue === null) {
+        valid = false;
+      }
+    }
+
+    return valid ? returnValue : defaultValue;
+  }
+
   static filter<T, R>(obj: T, props: string[]): R {
     return Object.keys(obj).reduce((result: R, key: string) => {
       const returnVal = result;
